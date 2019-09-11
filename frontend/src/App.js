a65cfbdedc78cab872b34e9d1ead09925e8296ed
bartoszkruba <bartosz.kruba@gmail.com>
2019-09-11 16:49:15 +0200

    Added navbar with login and register links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar/>
+        <div className="App">
 
-export default App;
+        </div>
+      </Fragment>
+
+    );
+  }
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
