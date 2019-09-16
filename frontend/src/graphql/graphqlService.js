cae588586a4f866a314c61058f09fd291f33cf8e
bartoszkruba <bartosz.kruba@gmail.com>
2019-09-16 19:43:37 +0200

    More graphqlService refactoring

--- frontend/src/graphql/graphqlService.js
+++ frontend/src/graphql/graphqlService.js
@@ -1,4 +1,6 @@
-import ApolloClient, {gql} from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
+import queries from "./queries";
+import mutations from './mutations'
 
 const URI = "http://localhost:8080/graphql";
 
@@ -16,50 +18,6 @@ const getClient = () => {
   return client;
 };
 
-const mutations = {
-  addNewFriend: gql`
-      mutation addFriend($emails: [String], $phoneNumbers: [String], $firstName: String!, $lastName: String!,
-          $city: String!, $country: String!, $timezone: String!){
-          addFriend(friendInput: {firstName: $firstName, lastName: $lastName,city: $city,country: $country,
-              timezone: $timezone, emails: $emails,phoneNumbers: $phoneNumbers
-          }){
-              firstName
-          }
-      }
-  `,
-  register: gql`
-      mutation register($email: String!, $password: String! ){
-          register(userInput: {email: $email, password: $password }){
-              _id
-              email
-          }
-      }
-  `
-};
-
-const queries = {
-  login: gql`
-      query login($email: String!, $password: String!){
-          login(email: $email, password: $password ){
-              token
-          }
-      }
-  `,
-  isAuthenticated: gql`
-      query{
-          isAuthenticated
-      }
-  `,
-  timezones: gql`
-      query {
-          timezones {
-              name
-          }
-      }
-  `
-};
-
-
 export default {
   addNewFriend: async friend => await getClient().mutate({
     mutation: mutations.addNewFriend, errorPolicy: "all", variables: {...friend}
@@ -74,32 +32,14 @@ export default {
   }),
 
   isAuthenticated: async () => await getClient().query({
-    query: queries.isAuthenticated
+    query: queries.isAuthenticated, errorPolicy: "all"
   }),
 
   timezones: async () => await getClient().query({
-    query: queries.timezones
+    query: queries.timezones, errorPolicy: "all"
   }),
 
   friends: async query => await getClient().query({
-    query: gql`
-        query {
-            friends(friendQuery: {
-                firstName: "${query.firstName}",
-                lastName: "${query.lastName}",
-                sort: "${query.sort}"
-            ${(query.from && query.to) ? `, from: "${query.from}", to: "${query.to}"`: ''}
-            }) {
-                _id
-                firstName
-                lastName
-                city
-                country
-                timezone {
-                    name
-                }
-            }
-        }
-    `
+    query: queries.friends, errorPolicy: "all", variables: {...query}
   })
 }
---
+++ frontend/src/graphql/mutations.js
@@ -0,0 +1,22 @@
+import {gql} from "apollo-boost";
+
+export default {
+  addNewFriend: gql`
+      mutation addFriend($emails: [String], $phoneNumbers: [String], $firstName: String!, $lastName: String!,
+          $city: String!, $country: String!, $timezone: String!){
+          addFriend(friendInput: {firstName: $firstName, lastName: $lastName,city: $city,country: $country,
+              timezone: $timezone, emails: $emails,phoneNumbers: $phoneNumbers
+          }){
+              firstName
+          }
+      }
+  `,
+  register: gql`
+      mutation register($email: String!, $password: String! ){
+          register(userInput: {email: $email, password: $password }){
+              _id
+              email
+          }
+      }
+  `
+}
