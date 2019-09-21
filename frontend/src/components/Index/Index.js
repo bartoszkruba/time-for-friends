815e5921e3c6602d3e390e19bb1c54cb6a968059
bartoszkruba <bartosz.kruba@gmail.com>
2019-09-21 16:54:57 +0200

    Switching text on index page depending on language props

--- frontend/src/components/Index/Index.js
+++ frontend/src/components/Index/Index.js
@@ -4,21 +4,36 @@ import {Button, Jumbotron} from 'reactstrap';
 import '../../App.css'
 
 
-const index = () => {
+const index = props => {
+
+  const text = {};
+
+  switch (props.language) {
+    case "se":
+      text.welcome = "Välkommen!";
+      text.button = "Registrera Dig Nu!";
+      text.lead = `"Time For Friends" är en webbapplikation som låter dig spåra dina vänners lokala tider.
+        Du kan ange så många personer med olika tidszoner som du vill och vi kommer spåra deras lokala tid åt dig helt gratis.`;
+      break;
+    case "en":
+      text.welcome = "Welcome!";
+      text.button = "Sign Up Now!";
+      text.lead = `"Time For Friends" is a web application that allows you to track your friends local time live.
+      You can enter as many people with different time zones as you want and we will track their local time for you 
+      totally for free.`;
+  }
 
   return <div className="container">
     <Jumbotron className="Card" style={{
       backgroundColor: "rgba(0, 0, 0, 0.8)", paddingRight: "100px",
       paddingLeft: "100px"
     }}>
-      <h1 className="display-3">Welcome!</h1>
-      <p className="lead">"Time For Friends" is an web application that allows you to track your friends local time live.
-        You can enter as many persons with different timezones as you want and we will track their local time for you
-        totally for free.</p>
+      <h1 className="display-3">{text.welcome}</h1>
+      <p className="lead">{text.lead}</p>
       <hr className="my-2"/>
       <p className="lead">
         <Link to="/register">
-          <Button color="info" size="lg" className="m-auto">Sign Up Now!</Button>
+          <Button color="info" size="lg" className="m-auto">{text.button}</Button>
         </Link>
       </p>
     </Jumbotron>
