aa6a3fce7c34ba2c32ef7423903a385662a82a1e
bartoszkruba <bartosz.kruba@gmail.com>
2019-09-28 13:45:38 +0200

    Added function for finding timezone depending on city and country to frontend

---
+++ frontend/src/components/NewFriendForm/findTimezone.js
@@ -0,0 +1,22 @@
+import tzLookup from 'tz-lookup'
+import cities from 'cities.json'
+
+export default (city, country) => {
+  let cityRegex = new RegExp(city, "i");
+  let countryRegex = new RegExp(country, "i");
+
+  const found = cities.find(city => {
+    return city.name.match(cityRegex) && city.country.match(countryRegex)
+  });
+
+  if (found) {
+    try {
+      return tzLookup(found.lat, found.lng)
+    } catch (e) {
+      console.log(e);
+      return "";
+    }
+  } else {
+    return ""
+  }
+}
